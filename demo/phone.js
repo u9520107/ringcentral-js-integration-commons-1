dc7e73ba9f4086bbcb2ce23be0881d0ed2227c1e
Jack Huang <u9520107@gmail.com>
2016-08-23 00:09:33 +0800

    progress

--- demo/phone.js
+++ demo/phone.js
@@ -3,13 +3,12 @@ import RcModule, { addModule } from '../src/lib/rc-module';
 import RingCentral from 'ringcentral';
 import { combineReducers, createStore } from 'redux';
 import Loganberry from 'loganberry';
-import { getProxyServer, getProxyClient, isProxy } from '../src/modules/rc-proxy';
-import uuid from 'uuid';
+import { getProxyServer, getProxyClient } from '../src/modules/rc-proxy';
 import Auth from '../src/modules/auth';
 import Subscription from '../src/modules/subscription';
 
-import EventEmitter from 'event-emitter';
-import Emitter from '../src/lib/emitter';
+import EventTransport from '../src/lib/event-transport';
+
 
 const logger = new Loganberry({
   prefix: 'demo',
@@ -17,96 +16,10 @@ const logger = new Loganberry({
 
 const REDUCER = Symbol();
 
-// the receiving emitter on client side
-const clientEmitter = new EventEmitter();
-
-// the receiving emitter on server side
-const serverEmitter = new EventEmitter();
-
-
-class EventTransport extends Emitter {
-  constructor(sourceEmitter, targetEmitter) {
-    super();
-    this.deferred = new Map();
-    sourceEmitter.on('message', (data) => {
-      try {
-        const payload = JSON.parse(data);
-        let deferred = null;
-        switch (payload.type) {
-          case 'rc-proxy-exec':
-            this.emit('exec', payload);
-            break;
-          case 'rc-proxy-exec-response':
-            deferred = this.deferred.get(payload.id);
-            if (!deferred) return;
-            if (payload.error) {
-              deferred.reject(payload.error);
-            } else {
-              deferred.resolve(payload.result);
-            }
-            break;
-          case 'rc-proxy-pipe-action':
-            this.emit('action', payload.action);
-            break;
-          default:
-            break;
-        }
-      } catch (e) {
-        logger.error(e);
-      }
-    });
-    this.targetEmitter = targetEmitter;
-    this.sourceEmitter = sourceEmitter;
-  }
-  exec(params) {
-    const payload = {
-      ...params,
-      type: 'rc-proxy-exec',
-      id: uuid.v4(),
-    };
-    logger.info(params);
-    const promise = new Promise((resolve, reject) => {
-      this.deferred.set(payload.id, {
-        resolve,
-        reject,
-      });
-    });
-
-    let timeout = setTimeout(() => {
-      timeout = null;
-      this.deferred.get(payload.id).reject(new Error('Proxied Execution Timeout'));
-    }, 30000);
-
-    promise.then((...args) => {
-      if (timeout) clearTimeout(timeout);
-      this.deferred.delete(payload.id);
-      return Promise.resolve(...args);
-    }, (...args) => {
-      if (timeout) clearTimeout(timeout);
-      this.deferred.delete(payload.id);
-      return Promise.reject(...args);
-    });
-
-    this.targetEmitter.emit('message', JSON.stringify(payload));
-    return promise;
-  }
-  execResponse(params) {
-    this.targetEmitter.emit('message', JSON.stringify({
-      ...params,
-      type: 'rc-proxy-exec-response',
-    }));
-  }
-  pipeAction(params) {
-    this.targetEmitter.emit('message', JSON.stringify({
-      ...params,
-      type: 'rc-proxy-pipe-action',
-      id: uuid.v4(),
-    }));
-  }
-  async sync(params) {
-
-  }
-}
+const transport = new EventTransport({
+  prefix: 'test',
+  timeout: 5,
+});
 
 class Phone extends RcModule {
   constructor(options) {
@@ -149,47 +62,49 @@ const promiseForServerStore = new Promise(resolve => {
 const server = new Server({
   apiSettings: config.sdk,
   promiseForStore: promiseForServerStore,
-  transport: new EventTransport(serverEmitter, clientEmitter),
+  transport,
 });
 
 serverStoreResolver(createStore(server.reducer));
+setTimeout(() => {
+  const Client = getProxyClient(Phone);
 
-const Client = getProxyClient(Phone);
+  let proxyStoreResolver = null;
+  const promiseForProxyStore = new Promise(resolve => {
+    proxyStoreResolver = resolve;
+  });
 
-let proxyStoreResolver = null;
-const promiseForProxyStore = new Promise(resolve => {
-  proxyStoreResolver = resolve;
-});
+  const proxy = new Client({
+    apiSettings: config.sdk,
+    promiseForStore: promiseForProxyStore,
+    transport,
+  });
 
-const proxy = new Client({
-  apiSettings: config.sdk,
-  promiseForStore: promiseForProxyStore,
-  transport: new EventTransport(clientEmitter, serverEmitter),
-});
+  proxyStoreResolver(createStore(proxy.reducer));
 
-proxyStoreResolver(createStore(proxy.reducer));
+  proxy.auth.isLoggedIn().then(loggedIn => {
+    console.log(proxy.auth.loginUrl({
+      redirectUri: 'localhost:8080/redirect',
+    }));
 
-proxy.auth.isLoggedIn().then(loggedIn => {
-  console.log(proxy.auth.loginUrl({
-    redirectUri: 'localhost:8080/redirect',
-  }));
-
-  proxy.subscription.subscribe(
-    '/restapi/v1.0/account/~/extension/~/presence?detailedTelephonyState=true'
-  );
-  proxy.subscription.on(
-    proxy.subscription.eventTypes.notification,
-    (message) => {
-      logger.info(message);
+    proxy.subscription.subscribe(
+      '/restapi/v1.0/account/~/extension/~/presence?detailedTelephonyState=true'
+    );
+    proxy.subscription.on(
+      proxy.subscription.eventTypes.notification,
+      (message) => {
+        logger.info(message);
+      }
+    );
+    if (!loggedIn) {
+      proxy.auth.login({
+        ...config.user,
+      });
     }
-  );
-  if (!loggedIn) {
-    proxy.auth.login({
-      ...config.user,
-    });
-  }
-}).catch(e => {
-  logger.error(e);
-});
+  }).catch(e => {
+    logger.error(e);
+  });
 
-global.proxy = proxy;
+  global.proxy = proxy;
+}, 5000);
+
--- src/modules/rc-proxy/index.js
+++ src/modules/rc-proxy/index.js
@@ -2,11 +2,11 @@ import SymbolMap from 'data-types/symbol-map';
 import RcModule, { initFunction, suppressInit } from '../../lib/rc-module';
 import Loganberry from 'loganberry';
 import { combineReducers } from 'redux';
-import { ActionMap } from '../../lib/redux-helper';
+import { ActionMap, prefixActions } from '../../lib/redux-helper';
 import uuid from 'uuid';
 
 const logger = new Loganberry({
-  prefix: 'rc-proxy',
+  prefix: 'proxy',
 });
 
 const symbols = new SymbolMap([
@@ -19,7 +19,7 @@ const symbols = new SymbolMap([
 
 const proxyActions = new ActionMap([
   'action',
-  'execResponse',
+  'execution',
   'sync',
 ], 'proxy');
 
@@ -38,9 +38,16 @@ export function proxify(prototype, property, descriptor) {
   function proxyFn(...args) {
     const functionPath = `${this.modulePath}.${property}`;
     logger.trace(`${functionPath} proxied`);
-    return this[symbols.transport].exec({
-      functionPath,
-      args,
+    return this[symbols.transport].request({
+      payload: {
+        type: `${this.prefix ? `${this.prefix}-` : ''}proxy-execution`,
+        functionPath,
+        args,
+      },
+    }).catch(error => {
+      const newError = new Error('Proxy execution timed out...');
+      newError.originalError = error;
+      return Promise.reject(newError);
     });
   }
   return {
@@ -81,15 +88,13 @@ export function throwOnProxy(prototype, property, descriptor) {
   };
 }
 
-export function isProxy() {
-  return !!this[symbols.transport];
-}
-
-
 export function getProxyServer(Module) {
   return class extends RcModule {
     constructor(options) {
-      super(options);
+      super({
+        ...options,
+        actions: proxyActions,
+      });
       this[symbols.module] = new Module({
         ...options,
         getState: () => this.state.module,
@@ -101,28 +106,48 @@ export function getProxyServer(Module) {
       if (!transport) {
         throw new Error('getProxyServer require transport to work...');
       }
-      transport.on('exec', async (payload) => {
+      transport.on(transport.events.request, async request => {
         const {
-          functionPath,
-          args,
-        } = payload;
-        // omit the root part of the path
-        const [_, ...pathTokens] = functionPath.split('.');
-        const fnName = pathTokens.pop();
-        let module = this[symbols.module];
-        pathTokens.forEach(token => {
-          module = module[token];
-        });
-        try {
-          const result = await module[fnName](...args);
-          transport.execResponse({
-            id: payload.id,
-            result,
+          requestId,
+          payload: {
+            type,
+            functionPath,
+            args,
+          },
+        } = request;
+
+        if (type === this.actions.execution) {
+          // omit the root part of the path
+          const [_, ...pathTokens] = functionPath.split('.');
+          const fnName = pathTokens.pop();
+          let module = this[symbols.module];
+          pathTokens.forEach(token => {
+            module = module[token];
           });
-        } catch (error) {
-          transport.execResponse({
-            id: payload.id,
-            error,
+          try {
+            const result = await module[fnName](...args);
+            transport.response({
+              requestId,
+              result,
+            });
+          } catch (error) {
+            transport.response({
+              requestId,
+              error,
+            });
+          }
+        } else if (type === this.actions.sync) {
+          transport.response({
+            requestId,
+            result: {
+              state: this.state.module,
+              timestamp: Date.now(),
+            },
+          });
+        } else {
+          transport.response({
+            requestId,
+            error: new Error(`request type '${type} not recognized`),
           });
         }
       });
@@ -130,9 +155,12 @@ export function getProxyServer(Module) {
       this[symbols.reducer] = combineReducers({
         module: this[symbols.module].reducer,
         proxy: (state, action) => {
-          transport.pipeAction({
-            action,
-            timestamp: Date.now(),
+          transport.push({
+            payload: {
+              type: `${this.prefix ? `${this.prefix}-` : ''}proxy-action`,
+              action,
+              timestamp: Date.now(),
+            },
           });
           return null;
         },
@@ -146,19 +174,46 @@ export function getProxyServer(Module) {
 
 
 function getProxyClientReducer(prefix, moduleReducer) {
+  const actions = prefixActions(proxyActions, prefix);
   return (state, action) => {
+    if (!state) {
+      return {
+        lastAction: null,
+        timestamp: 0,
+        module: moduleReducer(),
+      };
+    }
+    if (!action) {
+      return state;
+    }
     switch (action.type) {
+      case actions.action:
+        if (action.timestamp > state.timestamp) {
+          return Object.assign(
+            {},
+            state,
+            {
+              lastAction: action.action,
+              timestamp: action.timestamp,
+              module: moduleReducer(state.module, action.action),
+            },
+          );
+        }
+        return state;
+      case actions.sync:
+        if (action.state && action.timestamp > state.timestamp) {
+          return Object.assign(
+            {},
+            state,
+            {
+              module: action.state,
+              timestamp: action.timestamp,
+            }
+          );
+        }
+        return state;
       default:
-        return Object.assign(
-          {},
-          state,
-          {
-            module: moduleReducer(
-              state && state.module,
-              action,
-            ),
-          }
-        );
+        return state;
     }
   };
 }
@@ -235,19 +290,25 @@ export function getProxyClient(Module) {
           });
         }
       }
-
+      const {
+        transport,
+      } = options;
       // kick the module into proxied mode
-      if (!options.transport) {
+      if (!transport) {
         throw new Error('getProxyClient requires a transport object...');
       }
-      this[symbols.module]::setTransport(options.transport);
+      this[symbols.transport] = transport;
+      this[symbols.module]::setTransport(transport);
 
-      options.transport.on('action', async payload => {
-        const store = this.store || await options.promiseForStore;
-        store.dispatch({
-          ...payload,
-          type: this.actions.action,
-        });
+      transport.on(transport.events.push, async payload => {
+        if (payload.type === `${this.prefix ? `${this.prefix}-` : ''}proxy-action`) {
+          logger.trace(payload);
+          const store = this.store || await options.promiseForStore;
+          store.dispatch({
+            ...payload,
+            type: this.actions.action,
+          });
+        }
       });
 
       this[symbols.reducer] = getProxyClientReducer(this.prefix, this[symbols.module].reducer);
@@ -255,6 +316,16 @@ export function getProxyClient(Module) {
     @initFunction
     init() {
       this[symbols.module]::initProxy();
+      this[symbols.transport].request({
+        payload: {
+          type: `${this.prefix ? `${this.prefix}-` : ''}proxy-sync`,
+        },
+      }).then(result => {
+        this.store.dispatch({
+          ...result,
+          type: this.actions.sync,
+        });
+      });
     }
     get reducer() {
       return this[symbols.reducer];
